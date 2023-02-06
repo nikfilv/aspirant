e4e1a2f9a1db51b01867df1ac5aeed96cac27d25
Filipp Nikiforov <nikfilv@hotmail.com>
2023-02-06 15:05:52 +0200

    Реализация LU в numpy

---
+++ pedagogical_practice/numerical_methods/4.1.py
@@ -0,0 +1,25 @@
+import numpy as np
+from lu import decLU, solveLU
+n = 8
+A = -np.ones((n, n), 'float')
+
+for i in range(0, n):
+    A[i, i] = 1
+    A[i, n-1] = 1
+    if i < n-1:
+        A[i, i+1:n-1] = 0
+print('A:')
+print(A)
+
+LU = decLU(A)
+print('LU:')
+print(LU)
+
+f = np.ones((n), 'float')
+print('b:')
+print(f)
+
+
+x = solveLU(A, f)
+print('x:')
+print(x)
